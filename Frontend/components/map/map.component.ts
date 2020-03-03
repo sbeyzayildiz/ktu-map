162e893a751091542ea94f4111c3bb49af0f8143
sbeyzasahin <sumeyyebeyzasahin>
2020-03-03 16:40:40 +0300

    map zoom in out oluyor

--- Frontend/components/map/map.component.ts
+++ Frontend/components/map/map.component.ts
@@ -1,57 +1,50 @@
+import { createStringXY } from 'ol/coordinate';
+import MousePosition from 'ol/control/MousePosition';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import TileImage from 'ol/source/TileImage';
+import {defaults as defaultControls} from 'ol/control';
+import { fromLonLat } from 'ol/proj';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-
+  @ViewChild('olMap', {static: true}) olMapEl: ElementRef<HTMLDivElement>;
   constructor() { }
 
-  _view: View;
-  _map: Map;
-  // @ViewChild('olMap') olMap: ElementRef;
+  view: View;
+  map: Map;
+
   ngOnInit(): void {
-    this.createMap(document.getElementById('olMap'));
-    this.createBaseMaps();
+    this.createMap(this.olMapEl.nativeElement);
   }
 
   createMap(mapElement) {
-    this._view = new View({
-      center: [3900100, 4650000],
-      zoom: 6.8,
-      minZoom: 6,
-      maxZoom: 20,
-    });
-    this._map = new Map({
+    this.map = new Map({
       target: mapElement,
-      view: this._view
+     layers : [ new TileLayer({
+        source: new OSM()
+      })],
+      view: new View({
+        center: fromLonLat([39.7710, 40.9952]),
+        zoom: 17
+      }),
+      controls: defaultControls([]).extend([
+        new MousePosition({
+          coordinateFormat: createStringXY(4),
+          projection: 'EPSG:4326',
+          // comment the following two lines to have the mouse position
+          // be placed within the map.
+          className: 'custom-mouse-position',
+          target: document.getElementById(mapElement),
+          undefinedHTML: '&nbsp;'
+        })
+      ])
     });
-
-    this._map.getView().setMinZoom(6.8);
-
-  }
-
-  createBaseMaps() {
-    const googleRoad = new TileLayer({
-      source: new TileImage({ url: 'http://mt1.google.com/vt/lyrs=m@113&hl=tr&&x={x}&y={y}&z={z}' }),
-      visible: true
-    });
-    const googleHybrit = new TileLayer({
-      source: new TileImage({ url: 'http://mt1.google.com/vt/lyrs=y@113&hl=tr&&x={x}&y={y}&z={z}' }),
-      visible: false
-    });
-    const googleSat = new TileLayer({
-      source: new TileImage({ url: 'http://mt1.google.com/vt/lyrs=s@13&hl=tr&&x={x}&y={y}&z={z}' }),
-      visible: false
-    });
-    this._map.addLayer(googleRoad);
-    this._map.addLayer(googleHybrit);
-    this._map.addLayer(googleSat);
   }
 
 }
--- Frontend/components/src/app/app.module.ts
+++ Frontend/components/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from '../../map/map.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
