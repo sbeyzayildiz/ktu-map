0cadd3a822b23a27f1fef8be5c7848ad1cb4f895
sbeyzasahin <sumeyyebeyzasahin>
2020-03-04 17:45:38 +0300

    .

--- Frontend/components/src/app/app.module.ts
+++ Frontend/components/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MapComponent } from '../../map/map.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchBarComponent } from '../../search-bar/search-bar.component';
+import { ControlBarComponent } from './control-bar/control-bar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    SearchBarComponent,
+    ControlBarComponent
   ],
   imports: [
     BrowserModule,
